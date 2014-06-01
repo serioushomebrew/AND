7a97819156cf3aa0228dbf26edc4352503533d8c
Kevin van der Burgt <mail@kevinvdburgt.com>
2014-06-01 23:02:54 +0200

    Added some NNTP stuff

--- AND.js
+++ AND.js
@@ -1,2 +1,37 @@
-var config =    require('./config.json');
+var config      = require('./config.json'),
+
+    nntpClient  = new require('nntp')();
+
+
+nntpClient.on('ready', function() {
+    // Group
+    nntpClient.group('misc.test', function(err, count, low, high) {
+
+    });
+
+    // Article
+    nntpClient.article(function(err, n, id, headers, body) {
+
+    });
+
+    // Error
+    nntpClient.on('error', function(err) {
+        console.log('NNTP Client Error', err);
+    });
+
+    // Close
+    nntpClient.on('close', function() {
+        console.log('NNTP Client Close');
+    });
+
+    // Connect
+    nntpClient.connect({
+        host:       config.newsServer.host,
+        port:       config.newsServer.port,
+        secure:     config.newsServer.secure,
+        user:       config.newsServer.user,
+        password:   config.newsServer.password,
+        connTimeout:config.newsServer.timeout
+    });
+});
 
