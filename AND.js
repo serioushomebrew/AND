19c0347bb8096bf04f6ba728c5ecc8a784b28422
Timo de Jong <timodejong95@gmail.com>
2014-06-09 16:15:09 +0200

    The code is now using a custom class witch is easier to use

--- AND.js
+++ AND.js
@@ -1,41 +1,11 @@
-var config      = require('./config.json'),
+var config      = require('./config.json')
 
-    NNTP        = require('nntp'),
+    NNTP        = require('./library/nntp.js'),
 
-    nntpClient  = new NNTP();
+    NNTPClient  = new NNTP();
 
+NNTPClient.updateAll();
 
-
-nntpClient.on('ready', function() {
-    // Group
-    nntpClient.group('misc.test', function(err, count, low, high) {
-        console.log('GROUP');
-    });
-
-    // Article
-    nntpClient.article(function(err, n, id, headers, body) {
-        console.log('ARTICLE');
-    });
-
-    console.log('Ready');
-});
-
-// Error
-nntpClient.on('error', function(err) {
-    console.log('NNTP Client Error', err);
-});
-
-// Close
-nntpClient.on('close', function() {
-    console.log('NNTP Client Close');
-});
-
-// Connect
-nntpClient.connect({
-    host:       config.newsServer.host,
-    port:       config.newsServer.port,
-    secure:     config.newsServer.secure,
-    user:       config.newsServer.user,
-    password:   config.newsServer.password,
-    connTimeout:config.newsServer.timeout
-});
+// NNTPClient.updateSpots();
+// NNTPClient.updateComments();
+// NNTPClient.updateReports();
