d77294863816e5ac99d02c23cad835e1a92d4bc5
Timo de Jong <timodejong95@gmail.com>
2014-06-10 14:02:43 +0200

    Rewritten most of the code because it wasn't really dynamic

--- library/nntp.js
+++ library/nntp.js
@@ -2,9 +2,16 @@
  * TODO - Save the data in the database
  */
 
-var NNTPResource = require('nntp')
+var NNTPResource    = require('nntp')
 
-    NNTPEngine = new NNTPResource();
+    EventEmitter    = require('events').EventEmitter,
+
+    util            = require('util'),
+
+    inherits        = util.inherits
+
+    NNTPEngine      = new NNTPResource();
+
 
 function NNTP(config) {
     this.options = {
@@ -17,10 +24,12 @@ function NNTP(config) {
     }
     this.connected = false;
 }
+inherits(NNTP, EventEmitter);
 
-NNTP.prototype.connect = function(callback) {
+NNTP.prototype.connect = function() {
     var self = this;
 
+    // Make connection to the server
     NNTPEngine.connect({
         host:           this.options.host,
         port:           this.options.port,
@@ -30,136 +39,53 @@ NNTP.prototype.connect = function(callback) {
         connTimeout:    this.options.timeout
     });
 
+    // Wait till the connection is established, then emit "ready"
     NNTPEngine.on('ready', function() {
         self.connected = true;
-
-        callback();
+        self.emit('ready');
     });
+}
+
+NNTP.prototype.setGroup = function(groupName, callback) {
+    var self = this;
+
+    // Connect to the group
+    NNTPEngine.group(groupName, function(err, count, first, last) {
+        callback(err, count, first, last);
+    });
+}
+
+NNTP.prototype.readArticles = function(current, last, articleFunction, callback) {
+    var self = this;
+
+    // Check if we need to stop
+    if(current < last) {
+        NNTPEngine.article(current, function(err, nr, id, headers, body) {
+            articleFunction(err, current, id, headers, body);
+
+            // Call the next article
+            self.readArticles((current + 1), last, articleFunction, callback);
+        });
+    } else {
+        callback();
+    }
 }
 
 NNTP.prototype.disconnect = function() {
+    // Close connection with the server
     NNTPEngine.end();
-}
 
-NNTP.prototype.updateAll = function(lastSpotNr, lastCommentNr, lastReportNr) {
-    var self = this;
-
-    // open connection
-    self.connect(function () {
-
-        self.updateSpots(lastSpotNr, function() {
-            self.updateComments(lastCommentNr, function() {
-                self.updateReports(lastReportNr, function() {
-                   console.log('--- DONE WITH UPDATING ---');
-                    NNTPEngine.end();
-               });
-            });
-        });
+    // If the connection is closed, then emit "close"
+    NNTPEngine.on('close', function() {
+        self.emit('close');
     });
-};
-
-NNTP.prototype.updateSpots = function(lastSpotNr, callback) {
-    var self = this;
-
-    // open connection if needed
-    if(this.connected === false) {
-        self.connect(function() {
-            self.updateSpots(lastSpotNr, callback);
-        });
-    } else {
-        scanGroup('free.pt', lastSpotNr, function(err, nr, id, headers, body) {
-
-            console.log('Spot #' + nr);
-        }, callback);
-    }
-}
-
-NNTP.prototype.updateComments = function(lastCommentNr, callback) {
-    var self = this;
-
-    // open connection if needed
-    if(this.connected === false) {
-        self.connect(function() {
-            self.updateComments(lastCommentNr, callback);
-        });
-    } else {
-        scanGroup('free.usenet', lastCommentNr, function(err, nr, id, headers, body) {
-
-            console.log('Comment #' + nr);
-        }, callback);
-    }
-}
-
-NNTP.prototype.updateReports = function(lastReportNr, callback) {
-    var self = this;
-
-    // open connection if needed
-    if(this.connected === false) {
-        self.connect(function() {
-            self.updateReports(lastReportNr, callback);
-        });
-    } else {
-        scanGroup('free.willey', lastReportNr, function(err, nr, id, headers, body) {
-
-            console.log('Report #' + nr);
-            if (err) console.log(err);
-        }, callback);
-    }
 }
 
 
 module.exports = NNTP;
 
 
-// Call listeners
-// ERROR
+// Error listeners
 NNTPEngine.on('error', function(error) {
     console.log('Error: ' + error)
 });
-
-// CLOSE CONNECTION
-NNTPEngine.on('close', function(had_err) {
-    console.log('Connection closed');
-});
-
-// CONNECTION ENDED
-NNTPEngine.on('end', function() {
-    console.log('Connection ended');
-});
-
-
-function scanGroup(groupName, lastPosition, articleFunction, callback) {
-    NNTPEngine.group(groupName, function(err, count, first, last) {
-        if (err) console.log(err);
-
-        if(lastPosition != undefined)
-            first = lastPosition;
-
-        console.log('--- GET SPOTS [' + groupName + '] ---');
-        console.log('First Article: ' + first);
-        console.log('Last Article: ' + last);
-
-        if(first != last) {
-            console.log('--- Articles ---');
-
-            getArticles(first, first + 10, articleFunction, callback);
-        } else {
-            console.log('No new Spots');
-        }
-    });
-}
-
-function getArticles(current, last, articleFunction, callback) {
-
-    if(current < last) {
-        NNTPEngine.article(current, function(err, nr, id, headers, body) {
-            articleFunction(err, current, id, headers, body);
-
-            // Call the next article
-            getArticles((current + 1), last, articleFunction, callback);
-        });
-    } else {
-        console.log('--- DONE ---');
-        callback();
-    }
-}
